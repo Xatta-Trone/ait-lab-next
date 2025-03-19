4efc34d193a91fd74fad5bd508ec0c66de25b200
Gaurab Chhetri <contact@gaurabchhetri.com.np>
2025-03-19 10:09:06 -0500

    fix: outline button book-promotion

--- src/components/home-page/book-promotion.tsx
+++ src/components/home-page/book-promotion.tsx
@@ -36,7 +36,7 @@ export default function BookPromotion() {
                 }
                 target="_blank"
               >
-                <Button className="glass-button text-foreground px-6 py-2 rounded-full group">
+                <Button className="glass-button-book text-foreground px-6 py-2 rounded-full group">
                   <span>Order Here</span>
                   <ShoppingCart className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
                 </Button>
